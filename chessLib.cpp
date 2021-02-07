f5dea3b482687573391a968034ddf922987149e1
Ali DEREYURT <ali.dereyurt@stu.fsm.edu.tr>
2021-02-08 01:36:57 +0300

    .

--- chessLib.cpp
+++ chessLib.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+Tas **board = (Tas **)calloc(8, sizeof(Tas *));
+    for (i = 0; i < 8; i++) {
+        board[i] = (Tas *)calloc(8, sizeof(Tas));
+    }
 
 struct position
 {
@@ -18,21 +22,27 @@ struct position* Tas::getMove(){ return this->move; }
 Tas::Tas(struct position p1){
     Tas t1;
     t1.setPos(p1); };
+
 Sah::Sah(struct position p1){
     Sah s1;
     s1.setPos(p1); };
+    
 Vezir::Vezir(struct position p1){
     Vezir v1;
     v1.setPos(p1); };
+
 Fil::Fil(struct position p1){
     Fil f1;
     f1.setPos(p1); };
+
 At::At(struct position p1){
     At a1;
     a1.setPos(p1); };
+
 Kale::Kale(struct position p1){
     Kale k1;
     k1.setPos(p1); };
+
 Piyon::Piyon(struct position p1){
     Piyon piyon1;
     piyon1.setPos(p1); };
--- chessLib.h
+++ chessLib.h
@@ -82,3 +82,4 @@ public:
     ~Piyon();
     void printPiece(void);
 };
+
