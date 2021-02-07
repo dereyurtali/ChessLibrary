1c3ab96e204524e8c418a6ad224a540df2c76ee0
Ali DEREYURT <ali.dereyurt@stu.fsm.edu.tr>
2021-02-08 00:33:45 +0300

    contructors

--- chessLib.cpp
+++ chessLib.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+
 struct position
 {
     char dusey;
@@ -12,10 +13,26 @@ struct position
 
 void Tas::setPos(struct position p1){ this->pos = p1; }
 struct position Tas::getPos(){ return this->pos; }
-Tas::Tas(struct position p1){
-    Tas t1;
-    t1.setPos(p1);
-};
 void Tas::setMove(struct position *p1){ this->move = p1; }
 struct position* Tas::getMove(){ return this->move; }
-int a;
+Tas::Tas(struct position p1){
+    Tas t1;
+    t1.setPos(p1); };
+Sah::Sah(struct position p1){
+    Sah s1;
+    s1.setPos(p1); };
+Vezir::Vezir(struct position p1){
+    Vezir v1;
+    v1.setPos(p1); };
+Fil::Fil(struct position p1){
+    Fil f1;
+    f1.setPos(p1); };
+At::At(struct position p1){
+    At a1;
+    a1.setPos(p1); };
+Kale::Kale(struct position p1){
+    Kale k1;
+    k1.setPos(p1); };
+Piyon::Piyon(struct position p1){
+    Piyon piyon1;
+    piyon1.setPos(p1); };
